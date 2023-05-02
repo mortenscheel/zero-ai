3db2756973f9480a5031288a99700d3074629500
Morten Scheel <morten@hubhus.dk>
2023-05-02 13:08:43 +0200

    Customize image filename

--- app/Commands/Image.php
+++ app/Commands/Image.php
@@ -2,9 +2,7 @@
 
 namespace App\Commands;
 
-use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Process;
 use Illuminate\Support\Str;
 use LaravelZero\Framework\Commands\Command;
 use OpenAI\Client;
@@ -16,7 +14,7 @@ class Image extends Command
      *
      * @var string
      */
-    protected $signature = 'image {prompt*} {--size=256} {--print}';
+    protected $signature = 'image {prompt*} {--filename=}';
 
     /**
      * The description of the command.
@@ -36,27 +34,17 @@ class Image extends Command
         $response = $client->images()->create([
             'prompt' => $prompt,
             'n' => 1,
-            'size' => sprintf('%dx%d', $this->option('size'), $this->option('size')),
+            'size' => '256x256',
             'response_format' => 'url',
         ]);
         foreach ($response->data as $data) {
             $url = $data->url;
             $data = file_get_contents($url);
-            $filename = sprintf('%s.png', Str::slug($prompt));
+            $filename = $this->option('filename') ?: sprintf('%s.png', Str::slug($prompt));
             File::put(getcwd()."/$filename", $data);
-            if ($this->option('print')) {
-                Process::run(['imgcat', '--width=24', '--height=24', $filename]);
-            } else {
-                $this->info($filename);
-            }
+            $this->info($filename);
         }
-    }
 
-    /**
-     * Define the command's schedule.
-     */
-    public function schedule(Schedule $schedule): void
-    {
-        // $schedule->command(static::class)->everyMinute();
+        return self::SUCCESS;
     }
 }
